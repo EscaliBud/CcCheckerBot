7c76b983406f8a0c36dc3c44d69448e3fd5d82dd
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-12 13:26:05 +0530

    Update admincmd.php

--- admin/admincmd.php
+++ admin/admincmd.php
@@ -4,7 +4,7 @@ if ((strpos($message, "/adm") === 0)||(strpos($message, "!adm") === 0)||(strpos(
   $owners = file_get_contents('Database/owner.txt');
   $admins = explode("\n", $owners);
   if (!in_array($userId, $admins)) {
-      sendMessage($chatId,"Opps! You're not an Admin ❌",$messageId);
+      sendMessage($chatId,"Opps! You're not an Admin 🤣",$messageId);
   } else
   {
   sendMessage($chatId,urlencode(
