38efbbf437ee915c580bb92cba7bd17f6deb573c
ᴷʳᵉˢˢʷᵉˡˡ <147948046+EscaliBud@users.noreply.github.com>
2023-12-30 13:13:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$botToken = "6628835787:AAG3AEn1ukqUaENr09N5vk7S5odcO0BO5JM";
+$botToken = "6437949383:AAHaQQpzqD0U77CXfdTmjfA88MMSakjizzI";
 $website = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents('php://input');
 //echo $update;
